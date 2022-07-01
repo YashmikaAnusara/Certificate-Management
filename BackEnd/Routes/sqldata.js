e673b04915e521047a3da7aa0eea50849715b322
Thivanka Saparamadu <86546750+thiva99@users.noreply.github.com>
2022-07-01 12:56:59 +0530

    updated

---
+++ BackEnd/Routes/sqldata.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  host: "sql6.freesqldatabase.com",
+  user: "sql6503083",
+  password: "XIGq1irXCi",
+  database: "sql6503083",
+  port: 3306,
+});
+
+router.route("/requset").post((req, res) => {
+  const reqdata = req.body
+  pool.getConnection((err, connection) => {
+    try {
+      connection.query("INSERT INTO test SET ?", reqdata, (error) => {
+        connection.release();
+        if (error) {
+          console.log("this is try error");
+        } else {
+          res.json("Data Added");
+        }
+      });
+    } catch (e) {
+      console.log('this is a try catch error');
+    }
+  });
+});
+
+module.exports = router;

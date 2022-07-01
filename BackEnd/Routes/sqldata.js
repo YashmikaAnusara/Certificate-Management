497cbc2a4d45f20580cbab4b13d92e35ef0733aa
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-07-01 22:31:58 +0530

    Update sqldata.js

--- BackEnd/Routes/sqldata.js
+++ BackEnd/Routes/sqldata.js
@@ -10,8 +10,8 @@ const pool = mysql.createPool({
 });
 
 router.route("/requset").post((req, res) => {
-  const reqdata = req.body
-  pool.getConnection((err, connection) => {
+  const reqdata = req.body;
+  pool.getConnection((connection) => {
     try {
       connection.query("INSERT INTO request SET ?", reqdata, (error) => {
         connection.release();
@@ -22,7 +22,7 @@ router.route("/requset").post((req, res) => {
         }
       });
     } catch (e) {
-      console.log('this is a try catch error');
+      console.log("this is a try catch error");
     }
   });
 });

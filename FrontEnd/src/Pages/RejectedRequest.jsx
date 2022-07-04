8686bfa611309bbbe3627d3ff6d51160351834c9
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-07-05 02:51:04 +0530

    bug fix

--- FrontEnd/src/Pages/RejectedRequest.jsx
+++ FrontEnd/src/Pages/RejectedRequest.jsx
@@ -24,7 +24,7 @@ function RejectedRequest() {
                     {/* ------------------------------------------------------ */}
                     <div className='rejected-request-status-wrapper clearfix'>
                         <div><input type="search" placeholder='Search...' className='certificate-request-search' /> </div>
-                        <div className='rejected-request-status'> <FiberManualRecordIcon fontSize='small' style={{ color: "red" }} /><p style={{ marginLeft: "5px", fontSize: "14px", color: "black" }}>Rejected</p> </div>
+                        <div className='rejected-request-status'><div className='rejected'> <FiberManualRecordIcon fontSize='small' style={{ color: "red" }} /></div><p style={{ marginLeft: "5px", fontSize: "14px", color: "black" }}>Rejected</p> </div>
                     </div>
                     <div className='rejected-request-table-wrapper'>
                         <div className='rejected-request-table-header'>

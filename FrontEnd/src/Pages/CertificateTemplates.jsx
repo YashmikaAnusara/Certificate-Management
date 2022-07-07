9784290df3a89f5eecbf7e142923be86fff8ee46
Thivanka Saparamadu <86546750+thiva99@users.noreply.github.com>
2022-07-07 09:43:33 +0530

    Update CertificateTemplates.jsx

--- FrontEnd/src/Pages/CertificateTemplates.jsx
+++ FrontEnd/src/Pages/CertificateTemplates.jsx
@@ -11,29 +11,29 @@ import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DOCXIcon from "../Assets/doc.png";
 import FileDownload from "js-file-download";
-import Loader from '../Components/Loader'
+import Loader from "../Components/Loader";
 
 function CertificateTemplates() {
   const [active, setActive] = useState(false);
   const [details, setDetails] = useState([]);
-  const[isOpen,setIsOpen]=useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const tempAddhandler = () => {
     setActive(!active);
   };
 
   useEffect(() => {
-    setIsOpen(true)
+    setIsOpen(true);
     axios
       .get(`http://${Port}:8070/request/templates`)
       .then((res) => {
-        if(res.data){
-          setIsOpen(false)
+        if (res.data) {
+          setIsOpen(false);
           setDetails(res.data);
         }
       })
       .catch((err) => {
-        if(err){
-          setIsOpen(false)
+        if (err) {
+          setIsOpen(false);
           alert(err);
         }
       });
@@ -41,7 +41,7 @@ function CertificateTemplates() {
 
   return (
     <div className="container">
-      <Loader open={isOpen}/>
+      <Loader open={isOpen} />
       <div className="mob-navbar-wrapper">
         <MobNavBar />
       </div>
@@ -70,14 +70,14 @@ function CertificateTemplates() {
                 style={{ color: "red" }}
               />
             )}
-            {active ? <TemplateAdd setIsOpen={setIsOpen}/> : null}
+            {active ? <TemplateAdd setIsOpen={setIsOpen} /> : null}
           </div>
           <hr />
           <div className="certificate-temp-body-wrapper clearfix">
             <p className="certificate-temp-dis2">My Template</p>
             <div>
               {details.map((detail, index) => (
-                <Templates name={detail} key={index} setIsOpen={setIsOpen}/>
+                <Templates name={detail} key={index} setIsOpen={setIsOpen} />
               ))}
             </div>
           </div>
@@ -92,25 +92,25 @@ export default CertificateTemplates;
 
 function TemplateAdd(props) {
   const [file, setFile] = useState("");
-  const filename=file.name
+  const filename = file.name;
 
   const fileUploadHandler = () => {
     const data = new FormData();
     data.append("template", file);
     if (!file) {
       alert("Please select a file!");
-    } else if ((filename.split('.').pop())!=='docx') {
+    } else if (filename.split(".").pop() !== "docx") {
       alert("Please select a 'docx' format file!");
     } else {
-      props.setIsOpen(true)
+      props.setIsOpen(true);
       axios
         .post(`http://${Port}:8070/template/save`, data)
         .then(() => {
-          setTimeout(()=>{
-            props.setIsOpen(false)
+          setTimeout(() => {
+            props.setIsOpen(false);
             alert("Template added successful!");
             window.location.reload(false);
-          },2000)
+          }, 2000);
         })
         .catch((err) => {
           console.log(err);
@@ -143,40 +143,40 @@ function Templates(props) {
       "Are you sure want to remove this template?"
     );
     if (confirmBox === true) {
-      props.setIsOpen(true)
+      props.setIsOpen(true);
       axios
         .delete(`http://${Port}:8070/request/delete/template/${props.name}`)
         .then((res) => {
-          if(res.data){
-            props.setIsOpen(false)
+          if (res.data) {
+            props.setIsOpen(false);
             alert(res.data);
             window.location.reload(false);
           }
         })
         .catch((err) => {
-          if(err){
-            props.setIsOpen(false)
+          if (err) {
+            props.setIsOpen(false);
             alert(err);
           }
         });
     }
   };
   const tpmViewHandler = () => {
-    props.setIsOpen(true)
+    props.setIsOpen(true);
     axios({
       url: `http://${Port}:8070/request/template/${props.name}`,
       method: "GET",
       responseType: "blob",
     })
       .then((res) => {
-        if(res.data){
-          props.setIsOpen(false)
+        if (res.data) {
+          props.setIsOpen(false);
           FileDownload(res.data, `${props.name}`);
         }
       })
       .catch((err) => {
-        if(err){
-          props.setIsOpen(false)
+        if (err) {
+          props.setIsOpen(false);
           alert(err);
         }
       });
